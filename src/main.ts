fa2173e5056535eb2e2832802290919f386c7dfb
carlos-xbm <carlos.xbm@outlook.com>
2023-04-24 20:31:42 -0300

    pendecias atualizadas

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cors from 'cors';
 import { AppModule } from './app.module';
+
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    const cors = require('cors')
     app.use(cors())
     const config = new DocumentBuilder()
         .setTitle('Hiper Local')
