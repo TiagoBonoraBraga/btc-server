097c118d36df4b0afea730204669bf03b09f4766
FelipeCristo <felipe.cristo@yahoo.com.br>
2023-04-30 17:43:06 -0300

    Auth Login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiTags } from '@nestjs/swagger';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 @ApiTags('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+    @Post()
+  login(@Body() loginDto: LoginDto) {}
 }
