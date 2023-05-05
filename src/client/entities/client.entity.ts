a1a32a04d4113b9577299054e476ae6012877a90
carlos-xbm <carlos.xbm@outlook.com>
2023-05-05 19:42:42 -0300

    prisma ?

--- src/client/entities/client.entity.ts
+++ src/client/entities/client.entity.ts
@@ -2,9 +2,9 @@ import { Franchise, Product } from "@prisma/client";
 
 export class Client {
     id?: string;
-    name: string;
-    email: string;
-    phone: string;
+    name?: string;
+    email?: string;
+    phone?: string;
     cpf?: string;
     cnpj?: string;
     createdAt?: Date;
